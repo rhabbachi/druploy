d0c74c2d3abb324946605776338da987628531b9
Riadh Habbachi <habbachi.riadh@gmail.com>
2014-04-28 16:39:43 +0100

    Code refactoring.
    re #3 - I want to deploy to be renamed to deployment
    
    * Added task "deployement"
    * Refactor  task "deploy"
    * Some code clean up.

--- fabfile.py
+++ fabfile.py
@@ -186,34 +186,48 @@ def files(updating=True, resetting=False):
                 env.source_files = UpdateFromServerFilesSource(env.source_deployment)
             elif resetting == 'True':
                 env.source_files = ResetToSnapshotFilesSource(env.source_deployment)
-    except AttributeError, e:
+    except AttributeError:
         Utils.error("Source deployment not initialized properly, could not collect files")
-    except AgileProjectError, e:
+    except AgileProjectError:
         Utils.error("Could not collect files")
         abort("Ending")
 
 
 @task
-def deploy(name=None):
+def deployment(name=None):
     """
-    Run the deployment on the configured code, files and database
+    Tell Druploy what deployment to use.
     """
     execute(init)
     try:
         with hide(*env.clean):
+            # default name to branch name
+            if name is None:
+                name = env.source_code.branch
             env.deployment = ManagedDeployment(env.project, name)
             ManagedDeployment.create(env.deployment)
+    except AgileProjectError:
+        Utils.error("Error")
 
+
+@task
+def deploy():
+    """
+    Deploy using the configured code, files, database, and deployment.
+    """
+    execute(init)
+    try:
+        with hide(*env.clean):
+            if env.deployment is None:
+                deployment(None)
             env.deployment.code.source = env.source_code
             env.deployment.code.destination = CodeDeploymentDestination(env.deployment)
             env.deployment.files.source = env.source_files
             env.deployment.files.destination = FilesDestination(env.deployment)
             env.deployment.database.source = env.source_database
             env.deployment.database.destination = DatabaseDeploymentDestination(env.deployment)
-
             env.deployment.run()
-
-    except AgileProjectError, e:
+    except AgileProjectError:
         Utils.error("Could not deploy, ending")
         raise
 
@@ -226,12 +240,13 @@ def domain(name=None, aliases=[]):
     execute(init)
     try:
         with hide(*env.clean):
+            print(env.deployment)
             env.domain = Domain(env.deployment, name, aliases)
             env.domain.create()
             env.domain.enable()
-    except AttributeError, e:
+    except AttributeError:
         Utils.error("Source deployment not initialized properly, could not enable")
-    except AgileProjectError, e:
+    except AgileProjectError:
         Utils.error("Could not enable domain")
         abort("Ending")
 
@@ -254,8 +269,6 @@ def drush_alias_create():
                 drush_alias = DrushAlias(drupal_root)
                 DrushAlias.create(drush_alias)
 
-    except AgileProjectError, e:
+    except AgileProjectError:
         Utils.error("Could not sync aliases")
         raise
-
-
